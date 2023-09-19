faf9c36a5cc893399be0ee6fa91afd760e107afe
dekron_prime <admin@example.com>
2023-09-19 09:30:03 +0300

    lab4 done

---
+++ Lab4/src/main/java/org/example/ArrayProcessor.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public interface ArrayProcessor {
+    double calculateModuleOfSum(double[] array);
+    double calculate(double[][] array);
+    double calculateSqrtOfProduct(double[] array);
+    void processArray(double[] array);
+    void processArray(double[][] array);
+}
